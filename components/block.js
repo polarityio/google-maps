cced53f091f7ad486f130f8dfb4e75f141b52ca3
Ed <ed@polarity.io>
2019-03-26 12:44:54 -0400

    First pass fixes

--- components/block.js
+++ components/block.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 
 polarity.export = PolarityComponent.extend({
-    map: null,
-    onPropertiesSet: function(integrationBlockSource){
-        // there is no default action for this method but it can be overridden to track block information
-        this.set('isCollapsed', false);
-    },
-    initmap:  function() {
-        if(this.get('block.isCollapsed'))
-            return;
+  map: null,
+  blockCollapseChanged: Ember.computed("block.isCollapsed", function() {
+    if (this.get("block.isCollapsed")) {
+      return;
+    }
 
-        var map = this.get('map');
-        var entity = this.get('block.data.details');
+    Ember.run.scheduleOnce("afterRender", this, function() {
+      var map = this.get("map");
+      var entity = this.get("block.data.details");
 
-        if(map === null) {
-            var myOptions = {
-                scrollwheel:false,
-                navigationControl: false,
-                mapTypeControl: false,
-                scaleControl: true,
-                center: new google.maps.LatLng(entity.latitude, entity.longitude),
-                zoom: 13,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
+      if (map === null) {
+        var myOptions = {
+          scrollwheel: false,
+          navigationControl: false,
+          mapTypeControl: false,
+          scaleControl: true,
+          center: new google.maps.LatLng(entity.latitude, entity.longitude),
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-            map = new google.maps.Map(this.$('.geo-integration-map')[0], myOptions);
-            this.set('map', map);
-        }
+        map = new google.maps.Map(this.$(".geo-integration-map")[0], myOptions);
+        this.set("map", map);
+      }
 
-        google.maps.event.addListenerOnce(map, 'idle', function() {
-            google.maps.event.trigger(map, 'resize');
-            map.setCenter(new google.maps.LatLng(entity.latitude, entity.longitude));
-        });
-
-    }.observes('block.isCollapsed')
+      google.maps.event.addListenerOnce(map, "idle", function() {
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(
+          new google.maps.LatLng(entity.latitude, entity.longitude)
+        );
+      });
+    });
+  })
 });
